06341e93496c4059e4c2be266f5c1c157d04a8d4
ishikawa_shi <justlikebussiness@gmail.com>
2019-03-08 14:59:54 +0900

    Fix bugs

--- src/js/content_script.js
+++ src/js/content_script.js
@@ -1,22 +1,25 @@
-﻿import Unzip from 'unzip';
+﻿(function() {
+    console.log('Chap on!')
 
-(function() {
-    fetch('http://dl.stickershop.line.naver.jp/products/0/0/100/1/android/stickers.zip')
-    .then(
-        response => {
-            var unzip= new Unzip(new Buffer(response));
-            var filenames= unzip.getFilenames();
-            
-            for (let i = 0; i < filenames.length; i++) {
-                console.log(filenames);
-            }
-        }
-    ).catch(
-        error => console.log(error)
-    ); 
-
-    var menu = document.getElementById('home-message-entry-stamp-category-menu');
-    menu.appendChild('<li><a href="#" data-value="0"><img src="http://osouji-midoriarimatsu.com/wp-content/uploads/sites/145/LINE_icon_Green.png" alt="LINE1" title="LINE1"></a></li>');
-
+    const IMAGE_NUM = 18;
     
+    let $menu = $('#home-message-emoticon-tab-stamp');
+    let imageURL = chrome.extension.getURL(`icons/icon.png`);
+    let menuIcon = `<a href="#" class="neo-custon-menu" data-for="home-message-entry-stamp-category-menu"><span class="neo-select-menu-text"><img src="${imageURL}" alt="LINE" title="LINE"></span></a>`;
+    $menu.find('.app-page-buttonpane').append(menuIcon);
+
+    $('.neo-custon-menu').on('click', (e) => {
+        e.preventDefault();
+
+        let $stamp = $('#home-message-emoticon-tab-stamp');
+        $stamp.find('ul').html('');
+        for (let i = 1; i < IMAGE_NUM; i++) {
+            let imageURL = chrome.extension.getURL(`images/${i}.png`);
+    
+            let stampIcon = `<li data-category="0" data-id="1" data-url="${imageURL}" title="LINE${i}"><a href="#"><img src="${imageURL}" alt="LINE${i}"></a></li>`;
+            $stamp.find('ul').append(stampIcon);
+        }
+    });
+    $('.neo-custon-menu').find('img').width(24);
+    $('.neo-custon-menu').find('img').height(24);
 }).call(this);  
